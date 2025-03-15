bf093bfb5dd8dca827af59434a7d1f3c48f02cf6
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-03-15 11:41:33 +0400

    import tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Todo from "./components/Todo";
 export default function App() {
   return (
     <>
-      <h1 className="bg-red-100 text-white">home</h1>
-
+      <h1>home</h1>
       <Todo />
     </>
   );
