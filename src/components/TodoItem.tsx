485089e7e5691f5c08cee881df97ae7f8b5279ec
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-03-15 14:22:01 +0400

    delete todos

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,20 @@
-export default function TodoItem({ item }: { item: string }) {
-  return <h3>{item}</h3>;
+export default function TodoItem({
+  item,
+  todos,
+  setTodos,
+}: {
+  item: string;
+  todos: string[];
+  setTodos: any;
+}) {
+  function handleDelete(item: string) {
+    console.log("delete", item);
+    setTodos(todos.filter((todo: string) => todo !== item));
+  }
+  return (
+    <div className="flex justify-between w-[200px] bg-purple-200">
+      <h3>{item}</h3>
+      <button onClick={() => handleDelete(item)}>delete</button>
+    </div>
+  );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,15 @@
 import TodoItem from "./TodoItem";
-export default function TodoList({ todos }: { todos: string[] }) {
+export default function TodoList({
+  todos,
+  setTodos,
+}: {
+  todos: string[];
+  setTodos: any;
+}) {
   return (
     <div>
       {todos.map((item: string, index) => (
-        <TodoItem key={index} item={item} />
+        <TodoItem key={index} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
