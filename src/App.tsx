7e56916305051880647326ef6436e9cd4563e78d
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-03-15 11:36:51 +0400

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
 // import Hello from "./components/Hello";
-import Fruits from "./components/Fruits";
+import Todo from "./components/Todo";
 export default function App() {
-  // const person = {
-  //   name: "xing",
-  //   age: 14,
-  //   seatnumbers: [1, 2, 3],
-  // };
-  // const people = ["ana", "bob", "cat"];
-
-  const fruits = [
-    {
-      name: "apple",
-      price: 5,
-    },
-    {
-      name: "banana",
-      price: 2,
-    },
-
-    {
-      name: "mango",
-      price: 8,
-    },
-  ];
-
   return (
     <>
-      <h1>home</h1>
-      <ul>
-        {fruits.map((fruit) => {
-          return (
-            <Fruits key={fruit.name} name={fruit.name} price={fruit.price} />
-          ); // Return the JSX
-        })}
-      </ul>
+      <h1 className="bg-red-100 text-white">home</h1>
+
+      <Todo />
     </>
   );
 }
